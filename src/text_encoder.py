6fb6a3bacdc225635634803567e8db3f46f08afe
Yang Xianglin <yxlyzxl@gmail.com>
2025-05-27 21:01:18 +0800

    update

---
+++ src/text_encoder.py
@@ -0,0 +1,40 @@
+'''Implement text encoder using multiple embedding models'''
+
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+from sentence_transformers import SentenceTransformer
+
+class TextEncoder:
+    def __init__(self, model_name: str):
+        self.model_name = model_name
+    
+    def encode(self, text: str):
+        pass
+
+class XLMROBERTaTextEncoder(TextEncoder):
+    def __init__(self):
+        super().__init__("xlm-roberta-base")
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.model = AutoModelForMaskedLM.from_pretrained(self.model_name)
+    
+    def encode(self, text: str):
+        inputs = self.tokenizer(text, return_tensors="pt")
+        outputs = self.model(**inputs)
+        return outputs
+    
+
+class MiniLMTextEncoder(TextEncoder):
+    def __init__(self):
+        super().__init__("sentence-transformers/all-MiniLM-L6-v2")
+        self.model = SentenceTransformer(self.model_name)
+        # tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+        # model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+    
+    def encode(self, text: str):
+        text = [text]
+        return self.model.encode(text)[0]
+    
+    def batch_encode(self, texts: list[str]):
+        return self.model.encode(texts)
+    
+    
+    
