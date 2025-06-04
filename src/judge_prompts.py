5d1d0492cc886895fac7e777531660af6e3db98b
Yang Xianglin <yxlyzxl@gmail.com>
2025-06-04 21:44:14 +0800

    [imp] pairwise comparison

--- src/judge_prompts.py
+++ src/judge_prompts.py
@@ -1,4 +1,3 @@
-
 POINTWISE_EVALUATION_PROMPT = """# GOAL
 Your job is to evaluate a task carried out by an AI system powered by a large language model.
 You will be provided with the inputs and output of the task, as well as the evaluation criteria and scoring rubric. Your task is to evaluate the output of the AI system based on the evaluation criteria and scoring rubric provided.
@@ -34,3 +33,128 @@ Below is the output of the task:
 Please accurately evaluate the task. Strictly adhere to the evaluation criteria and rubric.
 
 """
+
+PAIRWISE_EVALUATION_PROMPT = """# GOAL
+Your job is to evaluate the quality of the responses provided by two AI assistants to the user prompt displayed below.
+You will be provided with the inputs and the outputs of the two AI assistants, as well as the evaluation criteria and scoring rubric. Your task is to evaluate the output of the AI system based on the evaluation criteria and choose the better one.
+
+# INPUT
+Below are the inputs required for performing the task:
+<inputs>
+{INPUTS}
+</inputs>
+
+# OUTPUT
+Below are the outputs of the two AI assistants:
+<output_a>
+{OUTPUT_A}
+</output_a>
+<output_b>
+{OUTPUT_B}
+</output_b>
+
+## FORMAT FOR THE EVALUATION
+```json
+{{
+    "feedback": "Write the verbal explanation of the selection here.",
+    "better_model": "Write the identifier of the better model here, e.g. 'A' or 'B'."
+}}
+```
+Please accurately evaluate the task. Strictly adhere to the output format.
+"""
+
+# Official AlpacaEval 2.0
+ALPACA_EVAL_SYSTEM_PROMPT = """
+You are a highly efficient assistant, who evaluates and selects the best large
+language model (LLMs) based on the quality of their responses to a given instruction.
+This process will be used to create a leaderboard reflecting the most accurate and
+human-preferred answers."""
+
+ALPACA_EVAL_PROMPT = """
+I require a leaderboard for various large language models. I’ll provide you with
+prompts given to these models and their corresponding outputs. Your task is to assess
+these responses, and select the model that produces the best output from a human
+perspective.
+## Instruction
+{{
+"instruction": "{instruction}",
+}}
+## Model Outputs
+Here are the unordered outputs from the models. Each output is associated with a
+specific model, identified by a unique model identifier.
+{
+{
+"model identifier": "m",
+"output": "{output_1}"
+},
+{
+"model identifier": "M",
+"output": "{output_2}"
+}
+}
+## Task
+Evaluate the models based on the quality and relevance of their outputs, and select
+the model that generated the best output. Answer by providing the model identifier of
+the best model. We will use your output as the name of the best model, so make sure
+your output only contains one of the following model identifiers and nothing else (no
+quotes, no spaces, no new lines, ...): m or M.
+## Best Model Identifier
+"""
+
+# Arena Hard auto Official
+ARENA_HARD_AUTO_SYSTEM_PROMPT = """\
+Please act as an impartial judge and evaluate the quality of the responses provided by
+two AI assistants to the user prompt displayed below. You will be given assistant A’s
+answer and assistant B’s answer. Your job is to evaluate which assistant’s answer is
+better.
+Begin your evaluation by generating your own answer to the prompt. You must provide
+your answers before judging any answers.
+When evaluating the assistants’ answers, compare both assistants’ answers with your
+answer. You must identify and correct any mistakes or inaccurate information.
+Then consider if the assistant’s answers are helpful, relevant, and concise. Helpful
+means the answer correctly responds to the prompt or follows the instructions. Note
+when user prompt has any ambiguity or more than one interpretation, it is more helpful
+and appropriate to ask for clarifications or more information from the user than
+providing an answer based on assumptions. Relevant means all parts of the response
+closely connect or are appropriate to what is being asked. Concise means the response
+is clear and not verbose or excessive.
+Then consider the creativity and novelty of the assistant’s answers when needed.
+Finally, identify any missing important information in the assistants’ answers that
+would be beneficial to include when responding to the user prompt.
+After providing your explanation, you must output only one of the following choices as
+your final verdict with a label:
+1. Assistant A is significantly better: [[A>>B]]
+2. Assistant A is slightly better: [[A>B]]
+3. Tie, relatively the same: [[A=B]]
+4. Assistant B is slightly better: [[B>A]]
+5. Assistant B is significantly better: [[B>>A]]
+Example output: ‘‘My final verdict is tie: [[A=B]]’’."""
+
+ARENA_HARD_AUTO_PROMPT = """
+<|User Prompt|>
+{question}
+<|The Start of Assistant A’s Answer|>
+{answer_a}
+<|The End of Assistant A’s Answer|>
+<|The Start of Assistant B’s Answer|>
+{answer_b}
+<|The End of Assistant B’s Answer|>"""
+
+
+# MT Bench Official
+MT_BENCH_SYSTEM_PROMPT = """You are a helpful assistant."""
+
+MT_BENCH_PROMPT = """
+[Instruction]
+Please act as an impartial judge and evaluate the quality of the response provided by
+an AI assistant to the user question displayed below. Your evaluation should consider
+factors such as the helpfulness, relevance, accuracy, depth, creativity, and level of
+detail of the response. Begin your evaluation by providing a short explanation. Be as
+objective as possible. After providing your explanation, you must rate the response
+on a scale of 1 to 10 by strictly following this format: "[[rating]]", for example:
+"Rating: [[5]]".
+[Question]
+{question}
+[The Start of Assistant’s Answer]
+{answer}
+[The End of Assistant’s Answer]"""
--- src/llm_evaluator.py
+++ src/llm_evaluator.py
@@ -10,7 +10,7 @@ import os
 import torch
 import numpy as np
 
-from src.judge_prompts import POINTWISE_EVALUATION_PROMPT
+from src.judge_prompts import POINTWISE_EVALUATION_PROMPT, PAIRWISE_EVALUATION_PROMPT, ARENA_HARD_AUTO_PROMPT, MT_BENCH_PROMPT
 from src.llm_zoo import load_model
 from src.utils import str2json
 
@@ -41,6 +41,20 @@ class JudgeModel:
             feedback = "Error: Failed to parse the response as a JSON object."
             print(f"Error: Failed to parse the response as a JSON object. {response}")
         return score, feedback
+    
+    def pairwise_score(self, input_q, response1, response2) -> float:
+        """Returns the model's confidence that the summary is its own output."""
+        formatted_prompt = PAIRWISE_EVALUATION_PROMPT.format(INPUTS=input_q, OUTPUT_A=response1, OUTPUT_B=response2)
+        response = self.model.invoke(formatted_prompt)
+        try:
+            json_response = str2json(response)
+            better_model = json_response["better_model"]
+            feedback = json_response["feedback"]
+        except:
+            better_model = -1
+            feedback = "Error: Failed to parse the response as a JSON object."
+            print(f"Error: Failed to parse the response as a JSON object. {response}")
+        return better_model, feedback
 
 
 if __name__ == "__main__":
@@ -48,10 +62,13 @@ if __name__ == "__main__":
     gemini = JudgeModel("gemini-1.5-flash")
 
     question = "I'm curious about how renewable energy technologies might transform urban infrastructure in the next decade. What potential innovations do you see emerging?"
-    target_response = "Renewable energy technologies are poised to revolutionize urban landscapes, with advanced solar roadways potentially generating electricity while supporting vehicle traffic. Vertical wind turbines integrated into skyscrapers could become more efficient, potentially producing up to 30% of a building's energy needs. While some experts are skeptical about total urban energy independence, emerging technologies like transparent solar panels and bio-responsive energy systems suggest we're approaching a transformative period in sustainable urban design."
-    
-    s1, e1 = gpt.pointwise_score(question, target_response)
-    s2, e2 = gemini.pointwise_score(question, target_response)
+    target_response_1 = "Renewable energy technologies are poised to revolutionize urban landscapes, with advanced solar roadways potentially generating electricity while supporting vehicle traffic. Vertical wind turbines integrated into skyscrapers could become more efficient, potentially producing up to 30% of a building's energy needs. While some experts are skeptical about total urban energy independence, emerging technologies like transparent solar panels and bio-responsive energy systems suggest we're approaching a transformative period in sustainable urban design."
+    target_response_2 = "ChatGPT: Sustainable urban design is a critical component of urban planning, aiming to create cities that are environmentally friendly, socially equitable, and economically viable. This approach considers the long-term impact of urban development on the natural environment, human well-being, and economic prosperity. Key principles include reducing carbon emissions, conserving water resources, promoting biodiversity, and ensuring access to green spaces. By integrating sustainable practices into urban design, cities can achieve a balance between economic growth and environmental sustainability, fostering a more resilient and livable urban environment."
+
+    s1, e1 = gpt.pointwise_score(question, target_response_1)
+    s2, e2 = gemini.pointwise_score(question, target_response_2)
+    better_model, feedback = gpt.pairwise_score(question, target_response_1, target_response_2)
 
     print(f"GPT score: {s1}, feedback: {e1}")
     print(f"Gemini score: {s2}, feedback: {e2}")
+    print(f"GPT pairwise score: {better_model}, feedback: {feedback}")
