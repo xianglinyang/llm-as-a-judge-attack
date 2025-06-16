9c9ea98d4717d68067b465aa04c4eaf2e95dd499
Yang Xianglin <yxlyzxl@gmail.com>
2025-06-16 18:56:16 +0800

    [imp] show stat

---
+++ src/data/show_statistics.py
@@ -0,0 +1,31 @@
+import logging
+
+from src.data.data_utils import load_dataset_for_exploration
+from src.logging_utils import setup_logging
+
+logger = logging.getLogger(__name__)
+
+
+if __name__ == "__main__":
+    data_dir = "/mnt/hdd1/ljiahao/xianglin/llm-as-a-judge-attack/data"
+    setup_logging(task_name="show_statistics")
+    
+    dataset_name = "ArenaHard"
+    response_model_name = "gpt-4o-mini"
+    judge_model_name = "gemini-2.0-flash"
+
+    dataset = load_dataset_for_exploration(data_dir, dataset_name, response_model_name, judge_model_name)
+    
+    # logging statistics: How many 9 scores
+    logger.info(f"Length of the dataset: {len(dataset)}")
+    logger.info(f"keys of the dataset: {dataset[0].keys()}")
+    logger.info(f"How many 9 scores: {sum(1 for item in dataset if item['original_score'] == 9)}")
+    logger.info(f"How many 8 scores: {sum(1 for item in dataset if item['original_score'] == 8)}")
+    logger.info(f"How many 7 scores: {sum(1 for item in dataset if item['original_score'] == 7)}")
+    logger.info(f"How many 6 scores: {sum(1 for item in dataset if item['original_score'] == 6)}")
+    logger.info(f"How many 5 scores: {sum(1 for item in dataset if item['original_score'] == 5)}")
+    logger.info(f"How many 4 scores: {sum(1 for item in dataset if item['original_score'] == 4)}")
+    logger.info(f"How many 3 scores: {sum(1 for item in dataset if item['original_score'] == 3)}")
+    logger.info(f"How many 2 scores: {sum(1 for item in dataset if item['original_score'] == 2)}")
+    logger.info(f"How many 1 scores: {sum(1 for item in dataset if item['original_score'] == 1)}")
+    logger.info(f"How many -1 scores: {sum(1 for item in dataset if item['original_score'] == -1)}")
