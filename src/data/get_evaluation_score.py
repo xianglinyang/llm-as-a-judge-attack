e450989716947688199a6552d84a5afb86f5fb52
Yang Xianglin <yxlyzxl@gmail.com>
2025-06-16 18:56:01 +0800

    update

--- src/data/get_evaluation_score.py
+++ src/data/get_evaluation_score.py
@@ -27,14 +27,14 @@ if __name__ == "__main__":
     data_dir = "/mnt/hdd1/ljiahao/xianglin/llm-as-a-judge-attack/data"
     
     # ------------------------------------------------------------
-    # get score from judge model and filter out the perfect score 9 samples
+    # get score from judge model
     # ------------------------------------------------------------
     setup_logging(task_name="preprocess_dataset_for_exploration")
     
     judge_model_list = [
-        "gemini-1.5-flash",
-        "gpt-4o",
-        "gpt-4.1",
+        "gemini-2.0-flash",
+        # "gpt-4o",
+        # "gpt-4.1",
     ]
     dataset_list = [
         "AlpacaEval",
@@ -58,20 +58,34 @@ if __name__ == "__main__":
                 # preprocess the dataset
                 question_list = [item['instruction'] for item in dataset]
                 response_list = [item['output'] for item in dataset]
-                category_list = [item['category'] for item in dataset]
-                original_score_list, original_explanation_list = asyncio.run(llm_evaluator.batch_pointwise_score(question_list, response_list))
+                original_score_list, original_explanation_list = llm_evaluator.batch_pointwise_score(question_list, response_list)
 
                 # construct the dataset for exploration
-                dataset_for_exploration = [
-                    {
-                        'instruction': question,
-                        'output': response,
-                        'category': category,
+                dataset_for_exploration = []
+                for item, original_score, original_explanation in zip(dataset, original_score_list, original_explanation_list):
+                    item.update({
                         'original_score': original_score,
                         'original_explanation': original_explanation,
-                    }
-                    for question, response, category, original_score, original_explanation in zip(question_list, response_list, category_list, original_score_list, original_explanation_list)
-                ]
+                    })
+                    dataset_for_exploration.append(item.copy())
+                
+                # save the dataset
+                save_path = os.path.join(data_dir, dataset_name, f"dataset_for_exploration_{response_model_name}_{judge_model_name}.json")
+                with open(save_path, "w") as f:
+                    json.dump(dataset_for_exploration, f, indent=4)
+                logger.info(f"Saved dataset to {save_path}")
+
+                # logging statistics: How many 9 scores
+                logger.info(f"How many 9 scores: {sum(1 for score in original_score_list if score == 9)}")
+                logger.info(f"How many 8 scores: {sum(1 for score in original_score_list if score == 8)}")
+                logger.info(f"How many 7 scores: {sum(1 for score in original_score_list if score == 7)}")
+                logger.info(f"How many 6 scores: {sum(1 for score in original_score_list if score == 6)}")
+                logger.info(f"How many 5 scores: {sum(1 for score in original_score_list if score == 5)}")
+                logger.info(f"How many 4 scores: {sum(1 for score in original_score_list if score == 4)}")
+                logger.info(f"How many 3 scores: {sum(1 for score in original_score_list if score == 3)}")
+                logger.info(f"How many 2 scores: {sum(1 for score in original_score_list if score == 2)}")
+                logger.info(f"How many 1 scores: {sum(1 for score in original_score_list if score == 1)}")
+                logger.info(f"How many -1 scores: {sum(1 for score in original_score_list if score == -1)}")
                 
                 # save the dataset
                 save_path = os.path.join(data_dir, dataset_name, f"dataset_for_exploration_{response_model_name}_{judge_model_name}.json")
