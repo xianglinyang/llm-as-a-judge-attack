d0b008af74da6c525c0fadd4c9dba27779f091de
Yang Xianglin <yxlyzxl@gmail.com>
2025-06-21 17:03:35 +0800

    minor

--- src/llm_zoo.py
+++ src/llm_zoo.py
@@ -375,6 +375,9 @@ def load_model(model_name: str, use_vllm: bool = False, **kwargs) -> ModelWrappe
         return OpenAIModel(model_name, **kwargs)
     elif "gemini" in model_name:
         available_models = [
+            "gemini-2.5-pro",
+            "gemini-2.5-flash",
+            "gemini-2.5-flash-lite-preview-06-17",
             "gemini-2.5-flash-preview-05-20",
             "gemini-2.5-pro-preview-06-05",
             "gemini-2.0-flash-lite",
