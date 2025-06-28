87e866b6c367eebfff187d6fdbc9b5a30bf892df
Yang Xianglin <yxlyzxl@gmail.com>
2025-06-28 08:53:24 +0800

    [imp] for mlrbench implementation

--- src/evolve_agent/bandit/UCB.py
+++ src/evolve_agent/bandit/UCB.py
@@ -203,7 +203,7 @@ if __name__ == "__main__":
     parser.add_argument("--Budget", type=int, default=20)
     parser.add_argument("--pool_size", type=int, default=3)
     parser.add_argument("--judge_model_name", type=str, default="gemini-2.0-flash")
-    parser.add_argument("--judge_type", type=str, default="pointwise", choices=["pointwise", "pairwise", "pairwise_fine_grained", "alpaca_eval", "arena_hard_auto", "mt_bench"])
+    parser.add_argument("--judge_type", type=str, default="pointwise", choices=["pointwise", "pairwise", "pairwise_fine_grained", "alpaca_eval", "arena_hard_auto", "mt_bench", "mlr_bench"])
     parser.add_argument("--answer_position", type=str, default="first", choices=["first", "second"], help="The position of the answer in the pairwise comparison")
     parser.add_argument("--baseline_response_model_name", type=str, default="gpt-4.1-mini")
     parser.add_argument("--llm_agent_name", type=str, default="gpt-4.1-nano")
