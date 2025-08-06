14d17c317cbf0003d231960fe1553e586826a523
Yang Xianglin <yxlyzxl@gmail.com>
2025-08-06 13:13:33 +0800

    [imp] parallel run

--- scripts/preprocessing/1-assign-categories.sh
+++ scripts/preprocessing/1-assign-categories.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+parallel_num=4
+
 data_dir="/mnt/hdd1/ljiahao/xianglin/llm-as-a-judge-attack/data"
 
 dataset_names=(
@@ -11,9 +13,17 @@ dataset_names=(
 
 assign_model="gpt-4.1-mini"
 
+counter=0
+
 for i in ${!dataset_names[@]}; do
     python -m src.data.assign_category \
         --dataset_name ${dataset_names[i]} \
         --assign_model_name ${assign_model} \
-        --data_dir ${data_dir}
+        --data_dir ${data_dir} &
+        
+    counter=$((counter + 1))
+    if [ $((counter % parallel_num)) -eq 0 ]; then
+        wait
+    fi
 done
+wait
--- scripts/preprocessing/2-get-response.sh
+++ scripts/preprocessing/2-get-response.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+parallel_num=4
+
 data_dir="/mnt/hdd1/ljiahao/xianglin/llm-as-a-judge-attack/data"
 
 dataset_names=(
@@ -12,6 +14,7 @@ response_models=(
     "gpt-4.1-mini"
 )
 
+counter=0
 
 for i in ${!dataset_names[@]}; do
     for j in ${!response_models[@]}; do
@@ -19,6 +22,12 @@ for i in ${!dataset_names[@]}; do
             --dataset_name ${dataset_names[i]} \
             --response_model_name ${response_models[i]} \
             --judge_backbone_name ${judge_backbones[i]} \
-            --data_dir ${data_dir} \
+            --data_dir ${data_dir} &
             # --use_vllm \
+        counter=$((counter + 1))
+        if [ $((counter % parallel_num)) -eq 0 ]; then
+            wait
+        fi
+    done
 done
+wait
