14e4e8515ed3441c0af1b77deecec1e49f58160f
Yang Xianglin <yxlyzxl@gmail.com>
2025-08-08 17:07:31 +0800

    [imp] script for running exp

--- scripts/preprocessing/2-get-response.sh
+++ scripts/preprocessing/2-get-response.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-parallel_num=4
+parallel_num=8
+
+# GPU Configuration
+per_gpu_jobs_num=1
+gpu_num=8
+jobs_num=$((per_gpu_jobs_num*gpu_num))
+gpu_ids=(0 1 2 3 4 5 6 7)
 
 data_dir="/mnt/hdd1/ljiahao/xianglin/llm-as-a-judge-attack/data"
 
@@ -11,19 +17,49 @@ dataset_names=(
     # "MLRBench"
 )
 response_models=(
-    "gpt-4.1-mini"
+    # "openai/gpt-4.1-mini"
+    # "gemini-1.5-flash-8b"
+    "Qwen/Qwen3-14B"
 )
+
+# ------------------------------------------------------------------------------------------------
+# api call
+# ------------------------------------------------------------------------------------------------
+
+# counter=0
+
+# for i in ${!dataset_names[@]}; do
+#     for j in ${!response_models[@]}; do
+#         python -m src.data.get_response_data \
+#             --dataset_name ${dataset_names[i]} \
+#             --response_model_name ${response_models[j]} \
+#             --data_dir ${data_dir} &
+            
+#         counter=$((counter + 1))
+#         if [ $((counter % parallel_num)) -eq 0 ]; then
+#             wait
+#         fi
+#     done
+# done
+# wait
+
+# ------------------------------------------------------------------------------------------------
+# vllm call
+# ------------------------------------------------------------------------------------------------
 
 counter=0
 
 for i in ${!dataset_names[@]}; do
     for j in ${!response_models[@]}; do
-        python -m src.data.get_response_data \
+
+        CUDA_VISIBLE_DEVICES=${gpu_ids[$((counter % jobs_num))]} python -m src.data.get_response_data \
             --dataset_name ${dataset_names[i]} \
-            --response_model_name ${response_models[i]} \
-            --judge_backbone_name ${judge_backbones[i]} \
+            --response_model_name ${response_models[j]} \
+            --use_vllm \
+            --tensor_parallel_size 1 \
+            --gpu_memory_utilization 0.95 \
             --data_dir ${data_dir} &
-            # --use_vllm \
+            
         counter=$((counter + 1))
         if [ $((counter % parallel_num)) -eq 0 ]; then
             wait
--- scripts/preprocessing/3-get-evaluation.sh
+++ scripts/preprocessing/3-get-evaluation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-parallel_num=4
+parallel_num=10
 
 data_dir="/mnt/hdd1/ljiahao/xianglin/llm-as-a-judge-attack/data"
 
@@ -11,11 +11,25 @@ dataset_names=(
     # "MLRBench"
 )
 response_models=(
-    "gpt-4.1-mini"
+    "openai/gpt-4.1-mini"
+    "gemini-1.5-flash-8b"
+    "Qwen/Qwen3-14B"
 )
+
+# Judge Models
 judge_models=(
-    "gemini-2.0-flash"
+    "google/gemini-2.5-flash"
+    "openai/o3"
+    "openai/o3-mini"
+    "google/gemini-2.5-pro"
+    "qwen/qwen3-235b-a22b-thinking-2507"
+    "qwen/qwen3-235b-a22b-2507"
+    "deepseek/deepseek-r1-0528"
+    "anthropic/claude-sonnet-4"
+    "meta-llama/llama-3.3-70b-instruct"
+    "qwen/qwen3-30b-a3b-instruct-2507"
 )
+
 
 judge_type="pointwise"
 
@@ -23,16 +37,18 @@ counter=0
 
 for i in ${!dataset_names[@]}; do
     for j in ${!response_models[@]}; do
-        python -m src.data.get_evaluation_score \
-            --dataset_name ${dataset_names[i]} \
-            --response_model_name ${response_models[i]} \
-            --judge_model_name ${judge_models[i]} \
-            --judge_type ${judge_type} \
-            --data_dir ${data_dir} &
-        counter=$((counter + 1))
-        if [ $((counter % parallel_num)) -eq 0 ]; then
-            wait
-        fi
+        for k in ${!judge_models[@]}; do
+            python -m src.data.get_evaluation_score \
+                --dataset_name ${dataset_names[i]} \
+                --response_model_name ${response_models[j]} \
+                --judge_model_name ${judge_models[k]} \
+                --judge_type ${judge_type} \
+                --data_dir ${data_dir} &
+            counter=$((counter + 1))
+            if [ $((counter % parallel_num)) -eq 0 ]; then
+                wait
+            fi
+        done
     done
 done
 wait
