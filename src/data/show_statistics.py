720d3501b2c88e87696b3d2bd261af298cbdd100
Yang Xianglin <yxlyzxl@gmail.com>
2025-08-12 11:06:45 +0800

    minor

--- src/data/show_statistics.py
+++ src/data/show_statistics.py
@@ -1,4 +1,5 @@
 import logging
+import numpy as np
 
 from src.data.data_utils import load_dataset_for_exploration
 from src.logging_utils import setup_logging
@@ -10,15 +11,16 @@ if __name__ == "__main__":
     data_dir = "/mnt/hdd1/ljiahao/xianglin/llm-as-a-judge-attack/data"
     setup_logging(task_name="show_statistics")
     
-    dataset_name = "MTBench"
-    response_model_name = "gpt-4o-mini"
-    judge_model_name = "gemini-2.0-flash"
+    dataset_name = "ArenaHard"
+    response_model_name = "gpt-4.1-mini"
+    judge_model_name = "qwen3-235b-a22b-2507"
 
     dataset = load_dataset_for_exploration(data_dir, dataset_name, response_model_name, judge_model_name)
     
     # logging statistics: How many 9 scores
     logger.info(f"Length of the dataset: {len(dataset)}")
     logger.info(f"keys of the dataset: {dataset[0].keys()}")
+    logger.info(f"Average Score: {float(np.mean([item['original_score'] for item in dataset if item['original_score'] != -1 and item['original_score'] != 9]))}")
     logger.info(f"How many 9 scores: {sum(1 for item in dataset if item['original_score'] == 9)}")
     logger.info(f"How many 8 scores: {sum(1 for item in dataset if item['original_score'] == 8)}")
     logger.info(f"How many 7 scores: {sum(1 for item in dataset if item['original_score'] == 7)}")
@@ -30,10 +32,22 @@ if __name__ == "__main__":
     logger.info(f"How many 1 scores: {sum(1 for item in dataset if item['original_score'] == 1)}")
     logger.info(f"How many -1 scores: {sum(1 for item in dataset if item['original_score'] == -1)}")
 
-    # Example element
-    logger.info(f"Example element:")
-    logger.info(f"Question: {dataset[0]['instruction']}")
-    logger.info(f"Original Response: {dataset[0]['output']}")
-    logger.info(f"Original Score: {dataset[0]['original_score']}")
-    logger.info(f"Original Explanation: {dataset[0]['original_explanation']}")
-    logger.info(f"Category: {dataset[0]['category']}")
+    # # show average scores of each question category excluding perfect samples
+    # categories = set([item['category'] for item in dataset])
+    # logger.info("-----------------------")
+    # logger.info("Average scores of each question category excluding perfect samples:")
+    # for category in categories:
+    #     category_samples = [item for item in dataset if item['category'] == category]
+    #     logger.info(f"{category}:\t{float(np.mean([item['original_score'] for item in category_samples if item['original_score'] != -1 and item['original_score'] != 9])):.2f}")
+    #     logger.info(f"Number of samples: {len(category_samples)}")
+    #     logger.info(f"Number of avaiable samples: {sum([1 for item in category_samples if item['original_score'] != -1 and item['original_score'] != 9])}")
+    #     logger.info(f"Number of perfect samples: {sum(1 for item in category_samples if item['original_score'] >= 9)}")
+    #     logger.info(f"--------------------------------")
+
+    # # Example element
+    # logger.info(f"Example element:")
+    # logger.info(f"Question: {dataset[0]['instruction']}")
+    # logger.info(f"Original Response: {dataset[0]['output']}")
+    # logger.info(f"Original Score: {dataset[0]['original_score']}")
+    # logger.info(f"Original Explanation: {dataset[0]['original_explanation']}")
+    # logger.info(f"Category: {dataset[0]['category']}")
